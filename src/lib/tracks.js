749f3faddcb01ed2962f98e452391dff8df117de
Thomas McEvoy <tmcevoy213@gmail.com>
2023-09-24 21:22:49 -0400

    mp3 asset path

--- src/lib/tracks.js
+++ src/lib/tracks.js
@@ -1,48 +1,48 @@
-import iila from '../assets/If I Love Again.mp3';
-import inemm from '../assets/It Never Entered My Mind.mp3';
-import attya from '../assets/All the Things You Are.mp3';
-import m from '../assets/Mandjet.mp3';
-import bhbg from '../assets/Black Hat.mp3';
-import af from '../assets/April Fool.mp3';
+import iila from '/assets/If I Love Again.mp3';
+import inemm from '/assets/It Never Entered My Mind.mp3';
+import attya from '/assets/All the Things You Are.mp3';
+import m from '/assets/Mandjet.mp3';
+import bhbg from '/assets/Black Hat.mp3';
+import af from '/assets/April Fool.mp3';
 
 export default [
 	{
-		src: {iila},
+		src: { iila },
 		title: 'If I Love Again',
 		album: 'Standards',
 		number: 0
 	},
 
 	{
-		src: {inemm},
+		src: { inemm },
 		title: 'It Never Entered My Mind',
 		album: 'Standards',
 		number: 1
 	},
 
 	{
-		src: {attya},
+		src: { attya },
 		title: 'All the Things You Are',
 		album: 'Standards',
 		number: 2
 	},
 
-  {
-		src: {m},
+	{
+		src: { m },
 		title: 'Mandjet',
 		album: 'Compositions',
 		number: 3
 	},
 
-  {
-		src: {bhbg},
+	{
+		src: { bhbg },
 		title: 'Black Hat, Black Gloves',
 		album: 'Compositions',
 		number: 4
 	},
 
 	{
-		src: {af},
+		src: { af },
 		title: 'April Fool',
 		album: 'Compositions',
 		number: 5
